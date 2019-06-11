ef49f7f11d12880fe7e48c7944d31caa166d0f65
v-boy <visheshsingh16@gmail.com>
2019-06-11 18:25:31 -0500

    Get City API call

--- js/forecast.js
+++ js/forecast.js
@@ -1,1 +1,16 @@
 const key = 't14zRKpZvWL6Y5V9se49PXtqTikVmGhe';
+
+const getCity = async city => {
+  const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
+  const query = `?apikey=${key}&q=${city}`;
+
+  const url = base + query;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity('Bloomington')
+  .then(data => console.log('City:', data))
+  .catch(err => console.log(err.message));
