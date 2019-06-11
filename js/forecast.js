ff3dab38b3d36ffbf627000697aebfa7dc17e140
v-boy <visheshsingh16@gmail.com>
2019-06-11 18:37:53 -0500

    Get Weather API Call

--- js/forecast.js
+++ js/forecast.js
@@ -1,5 +1,16 @@
 const key = 't14zRKpZvWL6Y5V9se49PXtqTikVmGhe';
 
+// get weather information
+const getWeather = async id => {
+  const base = `http://dataservice.accuweather.com/currentconditions/v1/${id}`;
+  const query = `?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get ciy information
 const getCity = async city => {
   const base = `http://dataservice.accuweather.com/locations/v1/cities/search`;
   const query = `?apikey=${key}&q=${city}`;
@@ -12,5 +23,6 @@ const getCity = async city => {
 };
 
 getCity('Bloomington')
-  .then(data => console.log('City:', data))
+  .then(data => getWeather(data.Key))
+  .then(data => console.log(data))
   .catch(err => console.log(err.message));
