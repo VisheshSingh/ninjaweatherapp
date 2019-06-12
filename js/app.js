4296cb5f3fb4a49d4436ba552c7202d2eb53767f
v-boy <visheshsingh16@gmail.com>
2019-06-11 19:00:54 -0500

    Updating the UI

--- js/app.js
+++ js/app.js
@@ -1,4 +1,24 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = data => {
+  const cityDetails = data.cityDetails;
+  const weather = data.weather;
+
+  details.innerHTML = `
+  <h5 class="my-3">${cityDetails.EnglishName}</h5>
+  <div class="my-3">${weather.WeatherText}</div>
+  <div class="display-4 my-4">
+    <span>${weather.Temperature.Imperial.Value}</span>
+    <span>&deg;C</span>
+  </div>
+  `;
+
+  if (card.classList.contains('d-none')) {
+    card.classList.remove('d-none');
+  }
+};
 
 const updateCity = async city => {
   const cityDetails = await getCity(city);
@@ -17,6 +37,6 @@ cityForm.addEventListener('submit', e => {
   cityForm.reset();
 
   updateCity(city)
-    .then(data => console.log(data))
+    .then(data => updateUI(data))
     .catch(err => console.log(err.message));
 });
